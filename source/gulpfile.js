e34b59a60b30c93aecdd0f5848e59c12333490fb
BeijiYang <langyuyang@outlook.com>
2016-05-06 16:47:39 +0800

    a bug about gulpfile.js and 'git-build'

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer');
+var prefix = require('gulp-autoprefixer'); 
+var wrap = require('gulp-wrap');
+
+gulp.task('build', function(){
+   gulp.src('pages/*.html')
+       .pipe(wrap({src:'layout/default.html'}))
+       .pipe(gulp.dest('..'));
+ });
 
 function handleError(err) {
   console.log(err.toString());
   this.emit('end');
 }
+
 
 gulp.task('sass', function(){
   gulp.src('styles/main.scss')
