5ba44b15abd581e8afa309893c9a6ffd10864aa2
BeijiYang <langyuyang@outlook.com>
2016-05-19 20:57:10 +0800

    fix a bug

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,7 +1,39 @@
 var gulp = require('gulp');
+var consolidate = require('consolidate');
 var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer'); 
+var prefix = require('gulp-autoprefixer');
+consolidate.requires.lodash = require('lodash');
 var wrap = require('gulp-wrap');
+var browserSync = require('browser-sync');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
+gulp.task('imagemin', function(){
+  return gulp.src('./assets/*')
+    .pipe(imagemin({
+      progressive: true,
+      svoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('../images'));
+});
+
+gulp.task('browser-sync', ['build', 'sass', 'cp'],function(){
+    browserSync({
+        server: {
+            baseDir: '..'
+        }
+    });
+});
+
+gulp.task('cp', function () {
+  return gulp.src(['js/main.js','assets/*'], { base: '.' })
+         .pipe(gulp.dest('..'));
+});
+
+gulp.task('rebuild',['build'], function(){
+  browserSync.reload();
+});
 
 gulp.task('build', function(){
    gulp.src('pages/*.html')
@@ -9,27 +41,22 @@ gulp.task('build', function(){
        .pipe(gulp.dest('..'));
  });
 
+gulp.task('sass', function(){
+  gulp.src('styles/main.scss')
+      .pipe(sass()).on('error', handleError)
+      .pipe(prefix())
+      .pipe(gulp.dest('../styles'))
+      .pipe(browserSync.reload({stream:true}));;
+});
+
 function handleError(err) {
   console.log(err.toString());
   this.emit('end');
 }
 
-
-gulp.task('sass', function(){
-  gulp.src('styles/main.scss')
-      .pipe(sass()).on('error',handleError)
-      .pipe(prefix())
-      .pipe(gulp.dest('../styles'));
-});
-
-gulp.task('copy', function(){
-  gulp.src('index.html')
-      .pipe(gulp.dest('..'));
-});
-
 gulp.task('watch', function(){
-  gulp.watch(['*.html'],['copy']);
+  gulp.watch(['**/*.html'],['rebuild']);
   gulp.watch(['styles/*.scss'],['sass']);
 });
 
-gulp.task('default', ['sass','copy','watch']);
+gulp.task('default', ['browser-sync','watch']);
