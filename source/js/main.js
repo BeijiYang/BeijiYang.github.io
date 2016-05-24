7a36f849929d131f28fe24a5811fbcda088adbf3
BeijiYang <langyuyang@outlook.com>
2016-05-24 12:17:45 +0800

    add js

---
+++ source/js/main.js
@@ -0,0 +1,20 @@
+
+$(".app-footer a").each(function(){
+  var path = document.location.pathname.split('/');
+  var page = path[path.length - 1];//总是最后
+  var href = $(this).attr("href");
+  if(href === page){
+    $(this).addClass("active");
+    return false;
+  }
+});
+
+$(".sidebar a").each(function(){
+  var path = document.location.pathname.split('/');
+  var page = path[path.length - 1];
+  var href = $(this).attr("href");
+  if(page === href){
+    $(this).addClass("active");
+    return false;
+  }
+});
